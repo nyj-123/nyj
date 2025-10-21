c733981341db85a0f0ddb4192c5077a48f51f858
nyj-123 <2832252249@qq.com>
2025-10-21 23:10:11 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,14 @@
-//
-//
-//
+//202510302119
+//2832252249@qq.com
+//牛运杰
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
+    float a = 0;
+    printf("请输入摄氏温度: ");
+    scanf("%f",&a);
+    float b=(a*9)/5+32;
+    printf("对应的华氏温度:%.1f",b);
+
     return 0;
 }
