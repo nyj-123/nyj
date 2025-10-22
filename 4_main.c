6a0863a5dcb63e769c31755c8b4feca6ec6998f9
nyj-123 <2832252249@qq.com>
2025-10-22 21:45:23 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -6,8 +6,8 @@
 
 int main() {
     int a,b,c = 0;
-    printf("请输入三个整数(用逗号隔开，判断是否能组成三角形):");
-    scanf("%d,%d,%d",&a,&b,&c);
+    printf("请输入三个整数(用空格隔开，判断是否能组成三角形):");
+    scanf("%d %d %d",&a,&b,&c);
     if(a + b > c && b + c > a && a + c > b)
     {
     printf("可以组成三角形\n");
