ad68b78e2051b7ce84ce563e13f24159553b4078
nyj-123 <2832252249@qq.com>
2025-10-31 00:05:25 +0800

    Update 1_se.c

--- 1_se.c
+++ 1_se.c
@@ -4,25 +4,32 @@
 #include <stdio.h>
 
 int main() 
-{   
+{
     int n = 0;
     printf("请输入一个50以内的正整数:");
     scanf("%d", &n);
     
-    if (n < 1 || n > 50) {
-        printf("输入无效，请输入1-50之间的正整数\n");
+    // 检查输入范围
+    if (n <= 1 || n > 50) {
+        printf("密钥不安全，请重新输入");
         return 0;
     }
     
-    for (int i = 2; i < n; i++)
-    {   
-        if (n % i == 0)
-        {
-            printf("密码不安全，请重新输入\n");
+    // 特殊处理2（2是质数）
+    if (n == 2) {
+        printf("密钥安全，密码设置成功");
+        return 0;
+    }
+    
+    // 检查是否为质数
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            printf("密钥不安全，请重新输入");
             return 0;
         }
     }
     
-    printf("密钥安全，密码设置成功\n");
+    // 如果循环完成都没有返回，说明是质数
+    printf("密钥安全，密码设置成功");
     return 0;
 }
