f67b040fad2b3bd5fc1c524d1b6d73466781928a
nyj-123 <2832252249@qq.com>
2025-10-30 23:42:06 +0800

    Update 3_se.c

--- 3_se.c
+++ 3_se.c
@@ -3,25 +3,26 @@
 //牛运杰
 #include <stdio.h>
 
-
 int main() 
-{   int n = 0 ;
+{   
+    int n = 0;
     printf("请输入一个50以内的正整数:");
-    scanf("%d",&n);
-    int i = 2 ;
-    while (i < n)
-    {   if(n % i == 0 )
-        {printf("密码不安全，请重新输入");
-        return 0 ;
-        }
-        else (n % i != 0);
-        {
-        printf("密钥安全，密码设置成功");
-        return 0 ;
-        i++;}    
+    scanf("%d", &n);
+    
+    if (n <= 1 || n > 50) {
+        printf("输入无效，请输入1-50之间的正整数\n");
+        return 0;
     }
     
+    for (int i = 2; i < n; i++)
+    {   
+        if (n % i == 0)
+        {
+            printf("密码不安全，请重新输入\n");
+            return 0;
+        }
+    }
     
-
+    printf("密钥安全，密码设置成功\n");
     return 0;
 }
