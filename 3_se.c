d97ea1e021517d50d49aa262d5faafb8511a9de3
nyj-123 <2832252249@qq.com>
2025-10-30 23:24:23 +0800

    Update and rename 2_main.c to 3_se.c

---
+++ 3_se.c
@@ -0,0 +1,27 @@
+//202510302119
+//2832252249@qq.com
+//牛运杰
+#include <stdio.h>
+
+
+int main() 
+{   int n = 0 ;
+    printf("请输入一个50以内的正整数:");
+    scanf("%d",&n);
+    int i = 2 ;
+    while (i < n)
+    {   if(n % i == 0 )
+        {printf("密码不安全，请重新输入");
+        return 0 ;
+        }
+        else (n % i != 0);
+        {
+        printf("密钥安全，密码设置成功");
+        return 0 ;
+        i++;}    
+    }
+    
+    
+
+    return 0;
+}
